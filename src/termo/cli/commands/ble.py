fbb205135e45b795771a524dc954b777b4c3df2a
cacko <alex@cacko.net>
2024-05-17 02:21:21 +0100

    "add dependencies to environment.yml diff diff for ec72441-1.2.3.1-2.2-3.3-4.1.0.0-beta.1/1.1e2.1"

--- src/termo/cli/commands/ble.py
+++ src/termo/cli/commands/ble.py
@@ -1,84 +1,52 @@
+from calendar import c
 import logging
+from re import A
+
+import rich
 from termo.cli.cli import cli
 from typing_extensions import Annotated
 import asyncio
-from bluetooth_sensor_state_data import BluetoothData
+
 from bleak import BleakClient
-from home_assistant_bluetooth import BluetoothServiceInfo
-from thermopro_ble.parser import ThermoProBluetoothDeviceData
+from bleak.backends.device import BLEDevice
+from bleak.backends.characteristic import BleakGATTCharacteristic
 from bleak import BleakScanner
 
+
 mac = "c4:5a:11:b4:53:19"
-address = "E18B152D-E13D-4FB6-B1F6-72C620625F27"
-MODEL_NBR_UUID = "2A24"
-
-
-TP357_S_2 = BluetoothServiceInfo(
-    name="TP357S (5319)",
-    manufacturer_data={
-        61122: b'\x00)"\x0b\x01',
-        60866: b'\x00)"\x0b\x01',
-        60610: b'\x00)"\x0b\x01',
-        60354: b'\x00("\x0b\x01',
-        60098: b'\x00("\x0b\x01',
-        59842: b'\x00)"\x0b\x01',
-        59586: b'\x00("\x0b\x01',
-        59330: b'\x00("\x0b\x01',
-        59074: b'\x00("\x0b\x01',
-        58818: b'\x00("\x0b\x01',
-        58562: b"\x00'\"\x0b\x01",
-        58306: b'\x00("\x0b\x01',
-        58050: b'\x00("\x0b\x01',
-        57794: b'\x00)"\x0b\x01',
-        57538: b'\x00)"\x0b\x01',
-        57282: b'\x00)"\x0b\x01',
-        57026: b'\x00)"\x0b\x01',
-        56770: b'\x00)"\x0b\x01',
-        56514: b'\x00)"\x0b\x01',
-        56258: b'\x00)"\x0b\x01',
-        56002: b'\x00)"\x0b\x01',
-        55746: b'\x00*"\x0b\x01',
-        55490: b'\x00)"\x0b\x01',
-        55234: b'\x00*"\x0b\x01',
-        54978: b'\x00*"\x0b\x01',
-        54722: b'\x00*"\x0b\x01',
-        54466: b'\x00+"\x0b\x01',
-        54210: b'\x00-"\x0b\x01',
-        53954: b'\x00,"\x0b\x01',
-        53698: b'\x00/"\x0b\x01',
-        53442: b'\x001"\x0b\x01',
-        53186: b'\x00."\x0b\x01',
-        52930: b'\x00,"\x0b\x01',
-        52674: b'\x00,"\x0b\x01',
-        52418: b'\x00+"\x0b\x01',
-        52162: b'\x00*"\x0b\x01',
-        51906: b'\x00*"\x0b\x01',
-        51650: b'\x00*"\x0b\x01',
-        51394: b'\x00*"\x0b\x01',
-        51138: b'\x00*"\x0b\x01',
-        50882: b'\x00)"\x0b\x01',
-    },
-    service_uuids=[mac],
-    address=address,
-    rssi=-60,
-    service_data={},
-    source="local",
-)
-
+ADDRESS = "E18B152D-E13D-4FB6-B1F6-72C620625F27"
+uuid_write = "00010203-0405-0607-0809-0a0b0c0d2b11"
+uuid_read  = "00010203-0405-0607-0809-0a0b0c0d2b10"
 
 @cli.command()
 def room():
-    parser = ThermoProBluetoothDeviceData()
-    res = parser.update(TP357_S_2)
-    print(res)
+    pass
+    
+def to_result(data: bytearray):
+    temp = (data[3] + data[4] * 256) / 10
+    humid = data[5]
+    rich.print([temp, humid])    
+    
+def notification_handler(sender: BleakGATTCharacteristic, data: bytearray):
+    to_result(data)
+
 
     
 @cli.command()
 def open():
     async def main():
-        devices = await BleakScanner.discover()
-        for d in devices:
-            print(d.rssi)
-            print(d.address)
+        device: BLEDevice = None
+        while not device:
+            devices = await BleakScanner.discover()
+            device = next(filter(lambda x: x.address == ADDRESS, devices), None)
+        async with BleakClient(device) as client:
+            read = client.services.get_characteristic(uuid_read)
+            write = client.services.get_characteristic(uuid_write)
+            await client.start_notify(read, callback=notification_handler)
+            try: 
+                while True:
+                    await asyncio.sleep(0.5)
+            except KeyboardInterrupt:
+                await client.stop_notify(read)
 
     asyncio.run(main())
